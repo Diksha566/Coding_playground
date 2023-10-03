ea488a57af314de8a77992252b7bd91fe3100476
Diksha Jethwa <121545576+Diksha566@users.noreply.github.com>
2023-10-03 14:58:11 +0530

    Practice question

--- Counting_digits.java
+++ Counting_digits.java
@@ -1,3 +1,4 @@
+// Problem Description: We are given an n-digit number. The task is to find the number of digits in the number, provided that, n>0.
 public class Counting_digits{
           public static void main(String args[]){
                     int num = 567, count=0;
