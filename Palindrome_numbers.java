9d72bd9b4fec6da3b43409489cb557e06a7d2b78
Diksha Jethwa <121545576+Diksha566@users.noreply.github.com>
2023-10-03 14:59:44 +0530

    Practice question

--- Palindrome_numbers.java
+++ Palindrome_numbers.java
@@ -1,3 +1,6 @@
+// Problem Description: We are given an n-digit number. The task is to find if the number is palindrome or not, provided that, n>=0.
+//If the Reversed Number is equal to the original number, then the number is said to be a palindrome number, otherwise not.
+
 public class Palindrome_numbers {
           public static void main(String args[]){
                     int number = 66866;
