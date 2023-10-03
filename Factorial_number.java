f3dc58b5ed48d11f5872a0899cbb13aff485db52
Diksha Jethwa <121545576+Diksha566@users.noreply.github.com>
2023-10-03 15:17:01 +0530

    Practice question

---
+++ Factorial_number.java
@@ -0,0 +1,14 @@
+// Problem Description: We are given a number. The task is to find the factorial of the number.
+//The factorial (n!) of a number is the product of all the integers from 1 to that number, i.e, 
+//n! = 1*2*3*.....(n-1)*n
+public class Factorial_number{
+          public static void main(String args[]){
+                    int number = 5,factorial=1;
+                    for(int i=1; i<=5;i++){
+                             System.out.print(factorial=factorial*i); 
+
+                    }
+
+
+          }
+}
