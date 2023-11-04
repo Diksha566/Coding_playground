98a80061c3d6bb3017bca92abd61409ff5efc903
Diksha Jethwa <121545576+Diksha566@users.noreply.github.com>
2023-11-04 23:27:42 +0530

    brian kernighan algo (bit manipulation)

---
+++ Count_Set_Bits.java
@@ -0,0 +1,26 @@
+//  count the number of set bits ( 1's ) in the binary representation of any number.
+// naive method is- ot travel to every digit and count the no set bits. time complexty will be high
+
+// by Brian Kernigum algo it only run, the num of set digits are there. 
+
+import java.util.Scanner;
+
+public class Count_Set_Bits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the binary num");
+        int n = sc.nextInt();
+        System.out.println(Count_Set_Bits(n));
+    }
+
+    static int Count_Set_Bits(int n){
+        int res=0;
+        while(n>0){
+            n= n & (n-1);
+           // System.out.print(n+" ");
+            res++;
+        }
+
+        return res;
+    }
+}
